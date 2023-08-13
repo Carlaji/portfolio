2b678de867af13d2f72b1ca56a67c3f34913da22
Carla <u1971612@campus.eram.cat>
2023-08-13 20:24:35 +0200

    Start Page + Finish Homepage PC(add transition, finish links)

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+//HOMEPAGE PROJECTS
 let Projects = document.querySelectorAll(".home-project");
 Projects.forEach((project) => {
   project.addEventListener("mouseover", () => {
@@ -12,3 +13,25 @@ Projects[Projects.length - 1].classList.add("active");
 setTimeout(() => {
   Projects[3].classList.remove("active");
 }, 100);
+
+//HOMEPAGE FOUND ME
+function goToInstagram() {
+  let url = "https://instagram.com/carla.byte?igshid=OGQ5ZDc2ODk2ZA==";
+  window.open(url);
+}
+let instagram = document.querySelector("#instagram");
+instagram.addEventListener("click", goToInstagram);
+
+function goTolinkedin() {
+  let url = "https://www.linkedin.com/in/carla-jimenez-109852212/";
+  window.open(url);
+}
+let linkedin = document.querySelector("#linkedin");
+linkedin.addEventListener("click", goTolinkedin);
+
+function goTogithub() {
+  let url = "https://github.com/Carlaji";
+  window.open(url);
+}
+let github = document.querySelector("#github");
+github.addEventListener("click", goTogithub);
