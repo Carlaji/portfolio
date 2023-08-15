1a827b23c920b9ada2035d9f9c65ea3a980d2349
Carla <u1971612@campus.eram.cat>
2023-08-15 13:29:23 +0200

    About me (JS + Responsive)

--- src/scripts.js
+++ src/scripts.js
@@ -1,61 +1,83 @@
+let home = document.querySelector(".home");
 //HOMEPAGE PROJECTS
-let Projects = document.querySelectorAll(".home-project");
-Projects.forEach((project) => {
-  project.addEventListener("mouseover", () => {
-    Projects.forEach((el) => el.classList.remove("active"));
-    project.classList.add("active");
+if (home) {
+  let Projects = document.querySelectorAll(".home-project");
+  Projects.forEach((project) => {
+    project.addEventListener("mouseover", () => {
+      Projects.forEach((el) => el.classList.remove("active"));
+      project.classList.add("active");
+    });
   });
-});
-Projects[0].classList.remove("active");
+  Projects[0].classList.remove("active");
 
-Projects[Projects.length - 1].classList.add("active");
+  Projects[Projects.length - 1].classList.add("active");
 
-setTimeout(() => {
-  Projects[3].classList.remove("active");
-}, 100);
-
+  setTimeout(() => {
+    Projects[3].classList.remove("active");
+  }, 100);
+}
 //HOMEPAGE FOUND ME
-function goToInstagram() {
-  let url = "https://instagram.com/carla.byte?igshid=OGQ5ZDc2ODk2ZA==";
-  window.open(url);
-}
+
 let instagram = document.querySelector("#instagram");
-instagram.addEventListener("click", goToInstagram);
-
-function goTolinkedin() {
-  let url = "https://www.linkedin.com/in/carla-jimenez-109852212/";
-  window.open(url);
-}
 let linkedin = document.querySelector("#linkedin");
-linkedin.addEventListener("click", goTolinkedin);
-
-function goTogithub() {
-  let url = "https://github.com/Carlaji";
-  window.open(url);
-}
 let github = document.querySelector("#github");
-github.addEventListener("click", goTogithub);
+
+let instagramurl = "https://instagram.com/carla.byte?igshid=OGQ5ZDc2ODk2ZA==";
+let linkedinurl = "https://www.linkedin.com/in/carla-jimenez-109852212/";
+let githuburl = "https://github.com/Carlaji";
+
+if (home) {
+  instagram.addEventListener("click", () => {
+    window.open(instagramurl, "_blank");
+  });
+
+  linkedin.addEventListener("click", () => {
+    window.open(linkedinurl, "_blank");
+  });
+
+  github.addEventListener("click", () => {
+    window.open(githuburl, "_blank");
+  });
+}
 
 //HOMEPAGE found me animation
-function clickBox() {
-  let box = document.querySelector("#box");
-  let found = document.querySelector("#found-title");
-  box.style.transform = "rotate(10deg)";
+if (home) {
+  function clickBox() {
+    let box = document.querySelector("#box");
+    let found = document.querySelector("#found-title");
+    box.style.transform = "rotate(10deg)";
 
-  setTimeout(() => {
-    box.style.transform = "rotate(-10deg)";
-  }, 1000);
-  setTimeout(() => {
-    instagram.hidden = false;
-    linkedin.hidden = false;
-    github.hidden = false;
-    box.style.transform = "rotate(0deg)";
-    found.innerHTML = "Found Me";
-  }, 3000);
+    setTimeout(() => {
+      box.style.transform = "rotate(-10deg)";
+    }, 1000);
+    setTimeout(() => {
+      instagram.hidden = false;
+      linkedin.hidden = false;
+      github.hidden = false;
+      box.style.transform = "rotate(0deg)";
+      found.innerHTML = "Found Me";
+    }, 3000);
+  }
+  instagram.hidden = true;
+  linkedin.hidden = true;
+  github.hidden = true;
+  let box = document.querySelector("#box");
+  box.addEventListener("click", clickBox);
 }
 
-instagram.hidden = true;
-linkedin.hidden = true;
-github.hidden = true;
-let box = document.querySelector("#box");
-box.addEventListener("click", clickBox);
+//ABOUT ME FOUND ME
+let about = document.querySelector(".about");
+if (about) {
+  let aboutinstagram = document.getElementById("aboutme-insta");
+  aboutinstagram.addEventListener("click", () => {
+    window.open(instagramurl, "_blank");
+  });
+  let aboutlinkedin = document.getElementById("aboutme-linkedin");
+  aboutlinkedin.addEventListener("click", () => {
+    window.open(linkedinurl, "_blank");
+  });
+  let aboutgithub = document.getElementById("aboutme-github");
+  aboutgithub.addEventListener("click", () => {
+    window.open(githuburl, "_blank");
+  });
+}
